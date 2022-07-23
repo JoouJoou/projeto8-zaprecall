dbc26e5ab9f5212e2cdcee37a5829535a478ce31
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-07-23 00:38:27 -0300

    Starting project & organazing archives

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles/reset.css";
+import "./styles/styles.css";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
